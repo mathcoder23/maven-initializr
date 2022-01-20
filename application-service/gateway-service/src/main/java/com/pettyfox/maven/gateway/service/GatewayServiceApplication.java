81d65d80b0302f6c991db59f1ff4c778054d9156
mathcoder23 <mathcoder23@126.com>
2022-01-20 21:31:04 +0800

    feat: init

---
+++ application-service/gateway-service/src/main/java/com/pettyfox/maven/gateway/service/GatewayServiceApplication.java
@@ -0,0 +1,22 @@
+package com.pettyfox.maven.gateway.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * <p>Copyright: Copyright (c) 2022</p>
+ * <p>Description: Created by Petty Fox on 2022/1/14</p>
+ * <p></p>
+ *
+ * @author Petty Fox
+ */
+@SpringBootApplication
+@ComponentScan({"com.pettyfox.maven"})
+public class GatewayServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayServiceApplication.class, args);
+    }
+
+}
